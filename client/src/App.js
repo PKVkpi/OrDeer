1a1ab32b53aa10bb585f41fffd2e2fe802d1a6de
Xasturr <dima.leshchenko20@gmail.com>
2020-11-23 23:17:49 +0200

    restaurants page added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/pages/home.component';
 import Signup from './components/pages/signup.component';
 import Login from './components/pages/login.component';
+import User from './components/pages/user.component';
+import Restaurants from './components/pages/restaurants.component';
 
 export default class App extends Component {
 
@@ -14,6 +16,8 @@ export default class App extends Component {
           <Switch>
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/user" component={User} />
+            <Route exact path="/restaurants" component={Restaurants} />
             <Route exact path="/" component={Home} />
           </Switch>
         </div>
---
+++ client/src/components/pages/restaurant-list.component.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Restaurant from "./restaurant.component";
+
+export default class RestaurantList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            restaurants: this.props.restaurants,
+        }
+    }
+
+    RestaurantList() {
+        return this.state.restaurants.map(restaurant => {
+            return (
+                <li className="restaurant__li" key={restaurant._id}>
+                    <Restaurant restaurant={restaurant} />
+                </li>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className="restaurant-list__list">
+                <ul className="restaurants__ul">
+                    {this.RestaurantList()}
+                </ul>
+            </div>
+        )
+    }
+}
